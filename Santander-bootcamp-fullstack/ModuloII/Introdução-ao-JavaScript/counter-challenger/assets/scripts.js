d0dd7f60ab64a8a80b60bcfb35ecd2b24fb3cc5c
OldRoge <rogeriomrjr@icloud.com>
2022-06-27 10:47:11 -0300

    refactor: refatorando funções para usar o addEventListener

--- Santander-bootcamp-fullstack/ModuloII/Introdução-ao-JavaScript/counter-challenger/assets/scripts.js
+++ Santander-bootcamp-fullstack/ModuloII/Introdução-ao-JavaScript/counter-challenger/assets/scripts.js
@@ -1,17 +1,17 @@
 let count = 0;
 
+const ELEMENT_BUTTON_SUM = document.getElementById('sum-button')
+
+const ELEMENT_BUTTON_SUB = document.getElementById('sub-button')
+
 const CURRENT_NUMBER = document.getElementById('currentNumber');
 
-function increment() {
-	count++;
+ELEMENT_BUTTON_SUM .addEventListener("click", () => {
+	count += 1;
 	CURRENT_NUMBER.innerHTML = count;
-}
+});
 
-function decrement() {
-	count--;
+ELEMENT_BUTTON_SUB.addEventListener('click', () => {
+	count -= 1;
 	CURRENT_NUMBER.innerHTML = count;
-}
-
-function test() {
-	kdowkdpo;
-}
+})
