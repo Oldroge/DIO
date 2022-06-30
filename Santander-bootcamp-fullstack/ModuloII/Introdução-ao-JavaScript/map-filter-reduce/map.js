ce0e79f26f4910b867f2ae9c0e6457767f0e1f55
OldRoge <rogeriomrjr@icloud.com>
2022-06-30 10:07:29 -0300

    study: exemplo de output com o método map

--- Santander-bootcamp-fullstack/ModuloII/Introdução-ao-JavaScript/map-filter-reduce/map.js
+++ Santander-bootcamp-fullstack/ModuloII/Introdução-ao-JavaScript/map-filter-reduce/map.js
@@ -9,6 +9,7 @@ const array = [1, 2, 3, 4, 5];
 const map = array.map((callback) => callback);
 
 console.log(map)
+// [ 1, 2, 3, 4, 5 ]
 
 // Dentro da callback ele recebe 3 valores:
 // valorAtual: o valor original da array
@@ -20,5 +21,11 @@ array.map((currentValue, index, array) => console.log(
   'index', index,
   'array', array
   ));
+// OUTPUT:
+// currentValue 1 index 0 array [ 1, 2, 3, 4, 5 ]
+// currentValue 2 index 1 array [ 1, 2, 3, 4, 5 ]
+// currentValue 3 index 2 array [ 1, 2, 3, 4, 5 ]
+// currentValue 4 index 3 array [ 1, 2, 3, 4, 5 ]
+// currentValue 5 index 4 array [ 1, 2, 3, 4, 5 ]
 
 // Por baixo dos panos faz a operação de maneira similar ao laço for, a iteração é medida pelo tamanho do array;
