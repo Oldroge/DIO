faa012d030d1ae79e1c3363d7bcfc246c1e634ba
OldRoge <rogeriomrjr@icloud.com>
2022-07-02 11:37:18 -0300

    study: aprendendo alguns conceitos de types e interfaces

--- Santander-bootcamp-fullstack/ModuloII/Introdução-ao-typescript/explorando-classes-tipos-interfaces/index.ts
+++ Santander-bootcamp-fullstack/ModuloII/Introdução-ao-typescript/explorando-classes-tipos-interfaces/index.ts
@@ -1,4 +1,34 @@
 function soma(a: number, b: number) {
   return a + b;
 };
-soma(1, '2');
+soma(1, 2);
+
+
+// Types
+// Interfaces
+
+interface IAnimal {
+  nome: string,
+  tipo: 'terrestre' | 'aquático',
+  executarRugido(alturaEmDecibeis: number): void;
+}
+
+interface IFelinos extends IAnimal {
+  visaoNoturna: boolean;
+
+}
+
+const animal: IAnimal = {
+  nome: 'Elefante',
+  tipo: 'terrestre',
+  executarRugido: (alturaEmDecibeis) => (`${alturaEmDecibeis}dB`),
+}
+
+animal.executarRugido(10);
+
+const felino: IFelinos = {
+  nome: 'Leao',
+  tipo: 'terrestre',
+  visaoNoturna: true,
+  executarRugido: (alturaEmDecibeis) => (`${alturaEmDecibeis}dB`),
+};
