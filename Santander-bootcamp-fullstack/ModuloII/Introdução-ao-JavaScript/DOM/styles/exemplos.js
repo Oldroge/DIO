c886ed6fa6283a7beb53492fc6786c41d1fca5df
OldRoge <rogeriomrjr@icloud.com>
2022-07-01 23:01:47 -0300

    study: vendo alguns conceitos de estilização utilizando o DOM

---
+++ Santander-bootcamp-fullstack/ModuloII/Introdução-ao-JavaScript/DOM/styles/exemplos.js
@@ -0,0 +1,10 @@
+const meuElemento = document.getElementById("meu-elemento");
+
+// método classList.add() adiciona uma nova classe ao elemento setado;
+meuElemento.classList.add('new-style');
+
+// método classList.remove() remove uma classe do elemento setado;
+meuElemento.classList.remove('classe');
+
+// método classList.toggle() adiciona uma classe caso ela não exista e caso ela exista remove;
+meuElemento.classList.toggle('darkMode');
