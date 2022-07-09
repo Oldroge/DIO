ee215db720f03faf36d03efb093fb08e47f6095e
oldroge <rogeriomrjr@gmail.com>
2022-07-09 00:09:47 -0300

    study: entendendo os benefícios de garantir o retorno de função

---
+++ Santander-bootcamp-fullstack/ModuloII/preparação-typescript/tipo-de-retorno/App.ts
@@ -0,0 +1,6 @@
+function somaValores(valor1: number, valor2: number): number {
+    return valor1 + valor2;
+};
+console.log(somaValores(1, 2));
+
+// Definindo o tipo de retorno da função é garantido que a função retornará o valor requerido, definindo apenas a entrada, garante apenas que a função aceite entradas de number ou qualquer outro tipo, porém não garante que o processo durante a função, não altere o seu retorno.
