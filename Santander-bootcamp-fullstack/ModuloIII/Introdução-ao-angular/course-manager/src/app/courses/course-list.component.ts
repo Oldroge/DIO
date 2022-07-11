b7f8a17c8d938a4cbd23a29af51a009f15dcdeb0
oldroge <rogeriomrjr@gmail.com>
2022-07-11 00:49:52 -0300

    study: estudando alguns conceitos de injeção de dependências

--- Santander-bootcamp-fullstack/ModuloIII/Introdução-ao-angular/course-manager/src/app/courses/course-list.component.ts
+++ Santander-bootcamp-fullstack/ModuloIII/Introdução-ao-angular/course-manager/src/app/courses/course-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Course } from "./course";
+import { CourseService } from "./course.service";
 // Dúvida
 @Component({
     // O selector será a tag correspondente no arquivo HTML pai;
@@ -12,30 +13,14 @@ export class CourseListComponent implements OnInit {
 
     courses: Course[] = [];
     // O ngOnInit é um local para colocar o código logo após que a classe for instanciada
-    ngOnInit(): void {
-        this.courses = [
-            {
-                id: 1,
-                name: 'Angular: forms',
-                imageUrl: '/assets/images/forms.png',
-                price: 99.99,
-                code: 'xps-8796',
-                duration: 120,
-                rating: 4.4,
-                releaseDate: 'december, 02, 2019',
-            },
-            {
-                id: 2,
-                name: 'Angular: HTTP',
-                imageUrl: 'assets/images/http.png',
-                price: 45.99,
-                code: 'lkl-1094',
-                duration: 80,
-                rating: 4.8,
-                releaseDate: 'November, 04, 2019'
 
-            }
-        ]
+    constructor(private courseService: CourseService) {
+
+    }
+
+    ngOnInit(): void {
+        this.courses = this.courseService.retrieveAll();
+        
     }
 
 
