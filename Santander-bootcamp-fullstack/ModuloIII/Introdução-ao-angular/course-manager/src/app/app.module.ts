c6bff092f49ceddbd0cfa3ea72524ada1a0a7b38
oldroge <rogeriomrjr@gmail.com>
2022-07-10 23:38:48 -0300

    study: criando outro componente e fazendo a conexão entre os componentes

--- Santander-bootcamp-fullstack/ModuloIII/Introdução-ao-angular/course-manager/src/app/app.module.ts
+++ Santander-bootcamp-fullstack/ModuloIII/Introdução-ao-angular/course-manager/src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { CourseListComponent } from './courses/course-list.component copy';
+import { CourseListComponent } from './courses/course-list.component';
+import { starComponent } from './star/star.component';
 // Na parte superior do documento, são feitos os imports
 
 @NgModule({
   // Todas as declarações de classes, ficam dentro da propriedade declarations
   declarations: [
     AppComponent,
-    CourseListComponent
+    CourseListComponent,
+    starComponent
   ],
   // Todos os imports feitos para dentro do módulo, ficam dentro desta propriedade
   imports: [
