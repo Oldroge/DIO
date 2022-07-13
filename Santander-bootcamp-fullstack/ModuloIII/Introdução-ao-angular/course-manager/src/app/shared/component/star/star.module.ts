155c5a8e54ba9e0de03c09653a9e798166b2fca3
oldroge <rogeriomrjr@gmail.com>
2022-07-13 18:15:19 -0300

    study: segrengando responsabilidades a outro modulo e utilizando a pasta shared

---
+++ Santander-bootcamp-fullstack/ModuloIII/Introdução-ao-angular/course-manager/src/app/shared/component/star/star.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from "@angular/core";
+import { starComponent } from "./star.component";
+
+@NgModule({
+    declarations: [
+        starComponent
+    ],
+    exports: [
+        starComponent
+    ]
+})
+
+export class starModule {
+    
+}
