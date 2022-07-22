f7a99b44e78de003357c79d4134b296121020507
oldroge <rogeriomrjr@gmail.com>
2022-07-21 23:45:33 -0300

    study: trabalhando com ngFor

--- Santander-bootcamp-fullstack/ModuloIII/preparando-ambiente-angular/angular-project/src/app/app.component.ts
+++ Santander-bootcamp-fullstack/ModuloIII/preparando-ambiente-angular/angular-project/src/app/app.component.ts
@@ -8,12 +8,29 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'Diretivas';
   counter = 1;
-  // allNum: number[] = [];
+  people = [
+    {
+      nome: 'Rogério',
+      sobrenome: 'Martins'
+    },
+    {
+      nome: 'Fabrício',
+      sobrenome: 'Gonçalves'
+    },
+    {
+      nome: 'Mariana',
+      sobrenome: 'Martins'
+    },
+    {
+      nome: 'Robson',
+      sobrenome: 'Amorim'
+    }
+  ];
 
   ngOnInit() {
     let increaseCounter = setInterval(() => {
       this.counter++;
-      // console.log(this.counter % 2 === 0)
+
       if (this.counter === 10) {
         clearInterval(increaseCounter);
       }
