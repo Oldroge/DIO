2b738671e0f32554f15bc9a8a7bd0b59d579b721
oldroge <rogeriomrjr@gmail.com>
2022-07-24 21:56:58 -0300

    study: criando um método para adicionar novo usuário

--- Santander-bootcamp-fullstack/ModuloIII/CRUD-com-angular/crud-dio-angular/src/app/services/user.service.ts
+++ Santander-bootcamp-fullstack/ModuloIII/CRUD-com-angular/crud-dio-angular/src/app/services/user.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from '../models/user';
@@ -8,10 +8,19 @@ import { User } from '../models/user';
 })
 export class UserService {
   apiUrl = 'https://sheet.best/api/sheets/06b4c1c4-037b-4c66-90a5-f9ff88885379';
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    })
+  }
 
   constructor(private httpClient: HttpClient) { }
 
   getUsers(): Observable<User[]> {
     return this.httpClient.get<User[]>(this.apiUrl)
+  }
+
+  postUser(user: User): Observable<User> {
+    return this.httpClient.post<User>(this.apiUrl, user, this.httpOptions)
   }
 }
