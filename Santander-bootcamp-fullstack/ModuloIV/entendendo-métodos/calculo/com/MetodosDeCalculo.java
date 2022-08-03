4afbd30aa35779359ecddca6d0f55923f021b65b
oldroge <rogeriomrjr@gmail.com>
2022-08-03 16:08:34 -0300

    study: exercícios de java, calculadora, saudação e emprestimo

---
+++ Santander-bootcamp-fullstack/ModuloIV/entendendo-métodos/calculo/com/MetodosDeCalculo.java
@@ -0,0 +1,23 @@
+package calculo.com;
+
+public class MetodosDeCalculo {
+    public void soma(int numA, int numB) {
+        System.out.println("Soma");
+        System.out.println(numA + numB);
+    }
+
+    public void subtrair(int numA, int numB) {
+        System.out.println("Subtração");
+        System.out.println(numA - numB);
+    }
+
+    public void multiplicar(int numA, int numB) {
+        System.out.println("Multiplicação");
+        System.out.println(numA * numB);
+    }
+
+    public void dividir(float numA, float numB) {
+        System.out.println("Divisão");
+        System.out.println(numA / numB);
+    }
+}
