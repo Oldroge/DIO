785f5c597c0637cee833b89b95c57eb84bbc6432
oldroge <rogeriomrjr@gmail.com>
2022-10-16 11:22:47 -0300

    study: concepts about functions and creating some

---
+++ cloud-fullstack-bootcamp/JavaScript/functions/challenger/name.js
@@ -0,0 +1,11 @@
+function writeMyName(name) {
+    console.log('My name is: ' + name);
+};
+
+function isMajor(age) {
+    if (age > 17) {
+        return 'You are of legal age'
+    } else {
+        return 'You are not of legal age'
+    }
+}
